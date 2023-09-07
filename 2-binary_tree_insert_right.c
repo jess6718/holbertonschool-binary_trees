d802d817c26156f04365420c4757086854779cf0
jess6718 <6718@holbertonstudents.com>
2023-09-06 22:25:38 -0700

    pass betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,24 +8,24 @@
   */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-        if (parent == NULL)
-        {
-                return (NULL);
-        }
-        new_node = binary_tree_node(parent, value);
-        if (new_node == NULL)/*malloc fails*/
-        {
-                return (NULL);
-        }
-        if (parent->right != NULL)/*if parent already has right child*/
-        {
-                /*set old child as child's right child*/
-                new_node->right = parent->right;
-                /*access old child's parent pointer and set as new_node*/
-                parent->right->parent = new_node;
-        }
-        parent->right = new_node;
-        return (new_node);
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)/*malloc fails*/
+	{
+		return (NULL);
+	}
+	if (parent->right != NULL)/*if parent already has right child*/
+	{
+		/*set old child as child's right child*/
+		new_node->right = parent->right;
+		/*access old child's parent pointer and set as new_node*/
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
+	return (new_node);
 }
