fa43efe221a451fc67e840d3ca4c0cbff2232032
jess6718 <6718@holbertonstudents.com>
2023-09-07 20:30:54 -0700

    update

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,7 +12,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return;
 	}
-	func(tree->n);
-	binary_tree_preorder(tree->right, func);
+	func(tree->n);/*process the value*/
+	/*make recursive call to left child till it reaches NULL*/
 	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
