d5be9df0dbb709efa3d940cf96b2c1fa396e34b9
jess6718 <6718@holbertonstudents.com>
2023-09-11 04:57:11 -0700

    test: task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+/**
+ * count_node - finds nodes of tree
+ * @tree: a pointer to the root node of tree
+ * Return: success returns total nodes of tree (including root), otherwise 0
+ */
+size_t count_node(const binary_tree_t *tree)
+{
+	size_t lh, rh;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	lh = count_node(tree->left);
+	rh = count_node(tree->right);
+	if (lh > rh)
+	{
+		/*return height of tree which is height of subtree + 1*/
+		return (lh + 1);
+	}
+	/*return height of tree which is height of subtree + 1*/
+	return (rh + 1);
+}
+/**
+ * binary_tree_is_full - checks if parent nodes have a L & R child
+ * @tree: pointer to root of binary tree
+ * Return: 1 on success, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (count_node(tree->left) == count_node(tree->right))
+	{
+		return (1);
+	}
+	return (0);
+}
